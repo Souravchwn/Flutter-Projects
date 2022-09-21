612a392ca56804e9f2f3762e36b6533ac9bd8063
souravchwn <souravchowhan31@gmail.com>
2022-09-21 09:02:16 +0600

    add files

--- lib/Screens/Login/components/login_form.dart
+++ lib/Screens/Login/components/login_form.dart
@@ -4,11 +4,16 @@ import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Signup/signup_screen.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
